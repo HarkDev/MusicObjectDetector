9ee8e3deec8afeafc14530985eb1f65eb09f88e9
Alexander Pacha <alexander.pacha@gmail.com>
2017-11-30 12:25:47 +0100

    Reduced complexity of muscima-pp-6 configuration

--- keras_frcnn/configurations/MuscimaPp6Config.py
+++ keras_frcnn/configurations/MuscimaPp6Config.py
@@ -5,12 +5,14 @@ from keras_frcnn.configurations.FasterRcnnConfiguration import FasterRcnnConfigu
 
 class MuscimaPp6Config(FasterRcnnConfiguration):
     def __init__(self):
-        super().__init__(anchor_box_scales=[1, 2, 4, 8, 16, 24],
+        super().__init__(anchor_box_scales=[4, 8, 16, 24],
                          anchor_box_ratios=[[1, 1],
                                             [1 / math.sqrt(4), 4 / math.sqrt(4)],
                                             [2 / math.sqrt(2), 1 / math.sqrt(2)]],
                          scale_images=False,
-                         number_of_ROIs_at_once=128)
+                         rpn_stride=8,
+                         number_of_ROIs_at_once=200,
+                         )
 
     def name(self) -> str:
         return "muscima_pp_6"
